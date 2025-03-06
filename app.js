ba537c15f602fb8dc4d41aadb8c835627ce73212
catozolala <catozolala@gmail.com>
2025-03-06 22:03:42 +0700

    update1

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+require("./config.js");
+
 const express = require("express");
 const path = require("path");
+const { Telegraf } = require("telegraf");
 
 const port = 3000
+const nyaa = new Telegraf(TOKEN_TELEGRAM);
 
 const app = express();
 
@@ -12,5 +16,21 @@ app.get("/", (req, res) => {
 });
 
 app.listen(process.env.PORT || port, () => {
+
+nyaa.telegram.sendMessage(CHAT_ID, "🚀 Sukses Menjalankan bot Telegram.... \nServer berjalan di https://abcd1234.ngrok.io", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "Salin Link",
+            url: "https://abcd1234.ngrok.io"
+          }
+        ]
+      ]
+    }
+  }).catch(error => {
+    console.error('Error sending message:', error);
+  });
+  
   console.log('Server berjalan pada port http://localhost:' + (process.env.PORT || port));
 });
